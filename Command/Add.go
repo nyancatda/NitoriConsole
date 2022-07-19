420abbb6f796730a1c1dab511413e310541f1a96
NyanCatda <nyancatda@outlook.com>
2022-07-19 16:21:47 +0800

    完成命令注册与执行

---
+++ Command/Add.go
@@ -0,0 +1,22 @@
+/*
+ * @Author: NyanCatda
+ * @Date: 2022-07-19 16:05:50
+ * @LastEditTime: 2022-07-19 16:15:23
+ * @LastEditors: NyanCatda
+ * @Description: 添加命令模块
+ * @FilePath: \NitoriConsole\Command\Add.go
+ */
+package Command
+
+/**
+ * @description: 注册一条命令
+ * @param {Command} string 命令名称
+ * @param {Help} string 命令帮助信息
+ * @param {Callback} func(string) 命令回调函数
+ * @return {*}
+ */
+func Add(Command string, Help string, Callback func(string)) {
+	// 添加至命令列表
+	CommandList[Command] = CommandInfo{Help, Callback}
+	return
+}
--- Command/Execution.go
+++ Command/Execution.go
@@ -1,13 +1,52 @@
 /*
  * @Author: NyanCatda
  * @Date: 2022-07-19 15:58:02
- * @LastEditTime: 2022-07-19 16:04:30
+ * @LastEditTime: 2022-07-19 16:19:28
  * @LastEditors: NyanCatda
  * @Description: 命令执行模块
  * @FilePath: \NitoriConsole\Command\Execution.go
  */
 package Command
 
-func Execution(Command string) error {
+import (
+	"errors"
+	"strings"
+)
 
+// 命令信息类型
+type CommandInfo struct {
+	Help     string
+	Callback func(string)
+}
+
+// 命令列表
+var CommandList map[string]CommandInfo
+
+/**
+ * @description: 执行命令
+ * @param {string} Command 命令字符串
+ * @return {error} 错误信息
+ */
+func Execution(Command string) error {
+	// 去除换行符
+	Command = strings.TrimSuffix(Command, "\n")
+
+	// 分割命令
+	CommandArray := strings.Fields(Command)
+	// 判断命令是否为空
+	if len(CommandArray) == 0 {
+		return nil
+	}
+
+	// 循环匹配命令列表
+	for CommandName, CommandInfo := range CommandList {
+		if CommandName == CommandArray[0] {
+			// 执行命令回调函数
+			CommandInfo.Callback(Command)
+			return nil
+		}
+	}
+
+	// 如果没有匹配到任何命令则返回命令不存在
+	return errors.New("Command not found")
 }
