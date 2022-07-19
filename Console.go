12ef0d380e2cc80500e316899ff40295a0e32173
NyanCatda <nyancatda@outlook.com>
2022-07-19 16:32:20 +0800

    增加是否在打印提示符号前换行参数

--- Console.go
+++ Console.go
@@ -1,7 +1,7 @@
 /*
  * @Author: NyanCatda
  * @Date: 2022-07-19 15:49:39
- * @LastEditTime: 2022-07-19 16:04:53
+ * @LastEditTime: 2022-07-19 16:25:41
  * @LastEditors: NyanCatda
  * @Description: 控制台模块
  * @FilePath: \NitoriConsole\Console.go
@@ -19,15 +19,19 @@ import (
 /**
  * @description: 启动控制台
  * @param {string} Prompt 提示符号，例如$
+ * @param {bool} Newline 是否在打印提示符号前换行
  * @param {func(error)} Error 错误回调函数
  * @return {*}
  */
-func Start(Prompt string, Error func(error)) {
+func Start(Prompt string, Newline bool, Error func(error)) {
 	Reader := bufio.NewReader(os.Stdin)
 	// 循环处理输入
 	for {
 		// 打印提示符号
-		fmt.Printf("\r" + Prompt)
+		if Newline {
+			fmt.Print("\n")
+		}
+		fmt.Print("\r" + Prompt)
 
 		// 获取输入
 		CmdString, err := Reader.ReadString('\n')
